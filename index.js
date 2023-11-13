9c01b0ce1639970ce0249279d985e8f824e2a8fc
JC-Coder <josephchimezie2003@gmail.com>
2023-11-13 05:18:24 +0100

    update codebase

--- index.js
+++ index.js
@@ -1,22 +1,43 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const { generateNewsFeed } = require('./server');
-const cron = require('node-cron');
 
 dotenv.config();
 
 const app = express();
 
 app.get('/ping', async (req, res) => {
-  return res.status(200).send({
-    message: 'pong',
-    data: new Date()
-  });
+  try {
+    return res.status(200).send({
+      success: true,
+      message: 'pong',
+      data: new Date()
+    });
+  } catch (e) {
+    console.error(e);
+    return res.status(500).send({
+      success: false,
+      message: 'Error Occurred'
+    });
+  }
 });
 
 app.get('/gen-xx-site', async (req, res) => {
-  await generateNewsFeed();
-  return res.send('Hello World');
+  try {
+    await generateNewsFeed();
+
+    return res.status(200).json({
+      success: true,
+      message: 'Success'
+    });
+  } catch (e) {
+    console.error(e);
+
+    return res.status(500).json({
+      success: false,
+      message: 'Error Occurred'
+    });
+  }
 });
 
 app.listen(process.env.PORT || 3000, () => {
