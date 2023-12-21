22525ca73ce0a99b77767f55553f1d9cc23ef99c
JC-Coder <josephchimezie2003@gmail.com>
2023-12-21 20:22:33 +0100

    update code

--- server.js
+++ server.js
@@ -2,17 +2,15 @@ const dotenv = require('dotenv');
 const OpenAI = require('openai');
 const axios = require('axios');
 const FormData = require('form-data');
-const {
-  samplePost1,
-  samplePost2,
-  samplePost3,
-  samplePost4
-} = require('./sampleContent');
+const { samplePost1 } = require('./sampleContent');
 
 dotenv.config();
 
+const coolDownDelay = 2000;
+
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
-const chatGptModel = 'text-davinci-003';
+// const chatGptModel = 'text-davinci-003';
+const chatGptModel = 'gpt-3.5-turbo-16k';
 
 function getNowDate() {
   const currentDate = new Date(); // Get the current date
@@ -116,39 +114,10 @@ async function getNewsArticles() {
   }
 }
 
-async function summarizeNewsArticles(articles) {
-  try {
-    return await Promise.all(
-      articles.map(async (article) => {
-        const prompt = `${article.title}. ${article.description}. ${article.content}. What kind of jobs will this create? The content size is at least 700 words.`;
-
-        const completion = await openai.completions.create({
-          model: chatGptModel,
-          prompt: prompt,
-          max_tokens: 3000,
-          temperature: 0
-        });
-
-        const summary = completion.choices[0].text.trim();
-
-        return {
-          title: article.title,
-          content: summary,
-          status: 'publish', // Set the status to 'publish' to publish the post immediately
-          imageUrl: article.urlToImage,
-          categories: article.categories
-        };
-      })
-    );
-  } catch (error) {
-    console.error('Error summarizing news articles:', error);
-  }
-}
-
 async function changeArticleTitle(originalTitle) {
   try {
     const response = await openai.completions.create({
-      model: chatGptModel,
+      model: 'text-davinci-003',
       prompt: `Rewrite the following article title to be similar but not the same:\n${originalTitle}\nNew Title:`,
       max_tokens: 1000,
       temperature: 0
@@ -162,25 +131,10 @@ async function changeArticleTitle(originalTitle) {
   }
 }
 
-async function summarizeNewsArticles2(articles) {
+async function summarizeNewsArticles(articles) {
   try {
     return await Promise.all(
       articles.map(async (article) => {
-        // const samplePostExcerpt = `${samplePost1}`;
-
-        // const instructions = `Using the style and structure of the above sample posts, create a blog article that first summarizes the content of the following article and then discusses how its themes relate to the jobs of the future. The blog post should have minimal sub-headings and be at least 700 words. note: don't include Blog Title : or Blog Content, i only need the content.`;
-
-        // const articleInfo = `${article.title}. ${article.description}. ${article.content}`;
-
-        // const prompt = `${samplePostExcerpt}\n\n${instructions}\n\nArticle Information:\n${articleInfo}`;
-
-        // const completion = await openai.completions.create({
-        //   model: chatGptModel,
-        //   prompt: prompt,
-        //   max_tokens: 3000,
-        //   temperature: 0
-        // });
-
         const samplePostExcerpt = `${samplePost1}`; // note: the sample post here is a sample of article style
         const instructions = `Using the style and structure of the above sample posts, create a blog article that first summarizes the content of the following article and then discusses how its themes relate to the jobs of the future. The blog post should be engaging and thought-provoking, aiming to retain the reader's interest throughout. It should provide not only a summary but also insightful elaboration on key points. Include relevant analogies, examples, and personal insights to make the content more relatable and share-worthy. The blog should encourage the reader to think deeply about the subject and feel compelled to share it for its value. The article should be less than 1500 words with minimal sub-headings. note: don't include Blog Title : or Blog Content, i only need the content.`;
 
@@ -188,16 +142,22 @@ async function summarizeNewsArticles2(articles) {
 
         const prompt = `${samplePostExcerpt}\n\n${instructions}\n\nArticle Information:\n${articleInfo}`;
 
-        const completion = await openai.completions.create({
-          model: chatGptModel,
-          prompt: prompt,
-          max_tokens: 3000,
-          temperature: 0.7 // Adjusted for creativity and variability
+        const completion = await openai.chat.completions.create({
+          model: 'gpt-4',
+          messages: [
+            {
+              role: 'user',
+              content: prompt
+            }
+          ],
+          temperature: 1,
+          max_tokens: 6000,
+          top_p: 1,
+          frequency_penalty: 0,
+          presence_penalty: 0
         });
 
-        const summary = completion.choices[0].text.trim();
-
-        console.log('summary', summary);
+        const summary = completion.choices[0].message;
 
         const newTitle = await changeArticleTitle(article.title);
 
@@ -214,6 +174,42 @@ async function summarizeNewsArticles2(articles) {
     console.error('Error summarizing news articles:', error);
     throw error;
   }
+}
+
+async function summarizeArticle(article) {
+  const samplePostExcerpt = `${samplePost1}`; // note: the sample post here is a sample of article style
+  const instructions = `Using the style and structure of the above sample posts, create a blog article that first summarizes the content of the following article and then discusses how its themes relate to the jobs of the future. The blog post should be engaging and thought-provoking, aiming to retain the reader's interest throughout. It should provide not only a summary but also insightful elaboration on key points. Include relevant analogies, examples, and personal insights to make the content more relatable and share-worthy. The blog should encourage the reader to think deeply about the subject and feel compelled to share it for its value. The article should be less than 1500 words with minimal sub-headings. note: don't include Blog Title : or Blog Content, i only need the content.`;
+
+  const articleInfo = `${article.title}. ${article.description}. ${article.content}`;
+
+  const prompt = `${samplePostExcerpt}\n\n${instructions}\n\nArticle Information:\n${articleInfo}`;
+
+  const completion = await openai.chat.completions.create({
+    model: 'gpt-4',
+    messages: [
+      {
+        role: 'user',
+        content: prompt
+      }
+    ],
+    temperature: 1,
+    max_tokens: 6000,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0
+  });
+
+  const summary = completion.choices[0].message.content;
+
+  const newTitle = await changeArticleTitle(article.title);
+
+  return {
+    title: newTitle,
+    content: summary,
+    status: 'publish', // Set the status to 'publish' to publish the post immediately
+    imageUrl: article.urlToImage,
+    categories: article.categories
+  };
 }
 
 async function pushToWebhook(blogPost) {
@@ -272,8 +268,9 @@ async function setFeaturedImage(imageUrl) {
 async function generateNewsFeed() {
   try {
     const newsArticles = await getNewsArticles();
+
     // const summaries = await summarizeNewsArticles(newsArticles);
-    const summaries = await summarizeNewsArticles2(newsArticles);
+    const summaries = await summarizeArticlesWithIntervals(newsArticles);
 
     await Promise.all(
       summaries.map(async (summary) => {
@@ -287,10 +284,38 @@ async function generateNewsFeed() {
   }
 }
 
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function summarizeArticlesWithIntervals(articles) {
+  const summarizedArticles = [];
+
+  console.log('summarizeArticlesWithIntervals started ===== ');
+
+  for (const article of articles) {
+    try {
+      const summary = await summarizeArticle(article);
+
+      summarizedArticles.push(summary);
+
+      // delay next summary
+      await sleep(coolDownDelay);
+    } catch (error) {
+      console.error('Error summarizing article : ', error);
+    }
+  }
+
+  console.log('summarizeArticlesWithIntervals completed ===== ');
+
+  return summarizedArticles;
+}
+
 // async function test() {
 //   const articles = await getNewsArticles();
 //   console.log('articles', articles);
-//   const summaries = await summarizeNewsArticles2(articles);
+
+//   const summaries = await summarizeArticlesWithIntervals(articles);
 //   console.log('summaries', summaries);
 // }
 
